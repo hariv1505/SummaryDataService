ac5024ccf49003e714582e054329081a9c9de882
hariAtThinkBright <hari@thinkbrightmedia.com.au>
2014-09-13 15:01:27 +1000

    More faults

--- src/au/edu/unsw/sltf/services/helper/MarketData.java
+++ src/au/edu/unsw/sltf/services/helper/MarketData.java
@@ -3,8 +3,10 @@ package au.edu.unsw.sltf.services.helper;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -50,7 +52,7 @@ public class MarketData {
 	}
 	
 	public MarketData(String sec2, Calendar startDate, Calendar endDate,
-			String dataSourceURL) {
+			String dataSourceURL) throws IOException {
 		this.sec = sec2;
 		this.startTime = startDate;
 		this.endTime = endDate;
@@ -62,8 +64,7 @@ public class MarketData {
 		return md;
 	}
 
-	private void URLtoMD(String dataSourceURL) {
-        try {
+	private void URLtoMD(String dataSourceURL) throws IOException {
         	URL dataURL = new URL(dataSourceURL);
 	        
 	        InputStream is = dataURL.openStream();
@@ -94,9 +95,6 @@ public class MarketData {
 	        }
 	        
 	        csvString = result;
-        } catch (Exception e) {
-        	//TODO
-        }
 	}
 
 	private Calendar getDateFromArray(String[] lineArray) {
